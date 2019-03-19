11d755b89690ae9761bfe94204ccaf04976f6954
Julia Zamaitat <j.zamaitat@gmail.com>
2019-03-19 11:17:57 +0100

    The basic businesscard class which you can use to work with variables.

---
+++ dienstag/src/BusinessCard.java
@@ -0,0 +1,23 @@
+public class BusinessCard {
+  public static void main(String[] args) {
+    businessCard();
+  }
+
+  public static void businessCard() {
+    System.out.println("+----------------------------------+");
+    System.out.println("| Julia Zamaitat                   |");
+    System.out.println("| Internationale Medieninformatik  |");
+    System.out.println("|                                  |");
+    System.out.println("| 3. Semester                      |");
+    System.out.println("| HTW Berlin                       |");
+    System.out.println("| Tutorin                          |");
+    System.out.println("+----------------------------------+");
+  }
+}
+
+/*
+    Aufgabe 1: Speichere die in der Visitenkarte vorhandenen Daten in Variablen ab. Nutze verschiedene Datentypen.
+                Achte darauf, möglichst verschiedene Datentypen zu nutzen.
+                Kommentiere jede Zeile Code und erkläre, was darin passiert.
+                Ändere die main Methode dann so ab, dass die Variablen in dem Output genutzt werden.
+*/
