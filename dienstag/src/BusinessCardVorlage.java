5ceabeb02705281a129ae8408893e105b2424417
Julia Zamaitat <j.zamaitat@gmail.com>
2019-03-19 11:25:21 +0100

    You can use this draft to create a Business card with variables.

---
+++ dienstag/src/BusinessCardVorlage.java
@@ -0,0 +1,27 @@
+public class BusinessCardVorlage {
+    //These are our "fields". Here we can declare and initalize variables. Please complete the fields as shown in the first example.
+
+    // Create avariable with the type string and give it a description/name. Assign your name.
+    //public static String name = "Julia Zamaitat";
+
+    // Create another variable with the type string and give it a description/name. Assign your study programme to it.
+    //public static ..
+
+    // Create another variable with the type int and give it a description/name. Assign your semester to it.
+    //public static int...
+
+    // Create a variable with the type string and give it a description/name. Assign the name of a street to it. Put a black space at the end within the string.
+   // public static..
+
+    // Create a variable with the type int and give it a description/name. Assign the street number of your address to it.
+    //public static ..
+
+    // This is the 'main' method. Everything starts from here. Call the businessCard method from here.
+    public static void main(String[] args) {
+        //YOUR CODE HERE
+    }
+
+    public static void businessCard() {
+       //YOUR CODE HERE – you should create your businessCard here and print the data using the variables
+    }
+}
