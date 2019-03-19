2ae6b2169398e5693f08703f9c9610e52f1bf7cd
Julia Zamaitat <j.zamaitat@gmail.com>
2019-03-19 11:36:28 +0100

    Added a draft for the calculator exercise.

---
+++ dienstag/src/CalculatorVorlage.java
@@ -0,0 +1,22 @@
+public class CalculatorVorlage {
+    public static void main(String[] args) {
+        //System.out.println("Das erwartete Ergebnis ist 15.");
+        //System.out.println("Das tatsächliche Ergebnis ist " + add(5, 10));
+        // Und so weiter:
+
+    }
+    //Calculate an addition here and return the result.
+    public static int add(int a, int b) {
+       //YOUR CODE HERE
+    }
+
+    //Calculate a substraction here and return the result.
+    public static int subtract(int a, int b) {
+       //YOUR CODE HERE
+    }
+
+    //COMPLETE multiplication: public static int multiply...
+
+    //COMPLETE division:  public static int divide...
+
+}
