03f8141376772ed95943ea552944428340b4f7dc
Julia Zamaitat <j.zamaitat@gmail.com>
2019-03-20 12:10:48 +0100

    Draft for your number comparison with the option to keep on playing.

---
+++ mittwoch/src/NumberComparisonVorlage.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NumberComparisonVorlage {
+    public static void main(String[] args) {
+        //boolean playing = true; //Makiert, ob man weiterspielen möchte.
+        //Loop, der so lange läuft, wie playing == true ist.
+        //while (...) {
+            //Wir brauchen ein Scanner Objekt, um User Input lesen zu können.
+            //Scanner scanner = ...
+            System.out.println("Gibt eine ganze Zahl ein: ");
+            //Der Computer wartet auf eine Eingabe, genauer genommen auf die nächste Zahl
+            //int ersteEingabe = ...
+            System.out.println("Gib eine zweite ganze Zahl ein. Ich werde errechnen, welche Zahl größer ist.");
+            //Der Computer wartet auf die zweite Zahl.
+            //int zweiteEingabe ...
+
+            //Hier wird entschieden, welche Zahl größer oder kleiner ist.
+            if (ersteEingabe > zweiteEingabe) {
+               //YOUR CODE
+           // } else if (...) {
+                //what happens then?
+            } //else ... (wenn Zahlen gleich groß)?
+
+            System.out.println("Willst du weitermachen? Antworte mit Ja oder Nein.");
+            //Hier wartet der Computer auf den nächsten String, der eingegeben wird.
+            //String choice = scanner.
+            //Die Antwort muss Ja oder Nein sein, sonst wird nochmal darauf hingewiesen
+            while(!choice.equals("Ja") && !choice.equals("Nein")){
+                //YOUR CODE
+            }
+            if (choice.equals("Ja")) //YOUR CODE
+            else //YOUR CODE
+        }
+    }
+
